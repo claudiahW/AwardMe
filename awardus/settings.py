1ce354f6295b9350b320accfe04008c4a488f94b
claudiahW <murungiclaud@gmail.com>
2021-12-09 22:09:08 +0300

    define basic app structure

--- awardus/settings.py
+++ awardus/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'weaward'
+    'bootstrap4',
+    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ weaward/urls.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from django.urls import path,include
+# from django.conf.urls import include,url
+from django.contrib.auth import views 
+from django_registration.backends.one_step.views import RegistrationView
