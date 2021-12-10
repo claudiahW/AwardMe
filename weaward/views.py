251a9eb3866595e405d7b2c6b40924a8a202e00b
claudiahW <murungiclaud@gmail.com>
2021-12-10 22:28:53 +0300

    render profile template

--- weaward/views.py
+++ weaward/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Profile,Project
 # Create your views here.
 
 def index(request):
