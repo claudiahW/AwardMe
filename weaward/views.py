db4f9cc4ea26b799a072b896842a8856ba3f17a1
claudiahW <murungiclaud@gmail.com>
2021-12-10 22:24:11 +0300

    define profile

--- weaward/views.py
+++ weaward/views.py
@@ -6,3 +6,12 @@ from django.http  import HttpResponse
 def index(request):
     
     return render(request, 'all-awards/index.html')
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    project = Project.objects.filter(user_id=current_user.id)
+
+    return render(request,"profile.html",{'profile':profile})
+
