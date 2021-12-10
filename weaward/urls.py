9bd43db8b13741695a0761fa705da4348b2c8e00
claudiahW <murungiclaud@gmail.com>
2021-12-10 22:04:43 +0300

    add project and profile class

--- weaward/urls.py
+++ weaward/urls.py
@@ -1,19 +1,10 @@
 
+
+
 from django.contrib import admin
 from django.urls import path,include
-# from django.conf.urls import include,url
-from django.contrib.auth import views 
-from django_registration.backends.one_step.views import RegistrationView
-
-
+from weaward import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('awardus.urls')),
-    path('accounts/register/', RegistrationView.as_view(success_url='/'),
-         name='django_registration_register'),
-    path('accounts/', include('django_registration.backends.one_step.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    
-    path('logout/', views.logout_then_login, name='logout'), 
+    path('',views.index,name= 'index'),
 ]
