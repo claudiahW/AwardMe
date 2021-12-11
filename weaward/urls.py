ba6511ba9123c3259a5f08548365ceb85fdb42f7
claudiahW <murungiclaud@gmail.com>
2021-12-11 17:21:34 +0300

    link the update function to the profile page

--- weaward/urls.py
+++ weaward/urls.py
@@ -3,11 +3,13 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from weaward import views 
+from weaward import views
+ 
 
 urlpatterns = [
     path('',views.index,name= 'index'),
     path('profile/', views.profile, name='profile'),
     path('accounts/profile/', views.profile,name='profile'),
     path('post/project/', views.post, name = "post"),
+    path('update_profile/',views.update_profile, name='update_profile'),
 ]
