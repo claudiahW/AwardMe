3a9fa03424b46ecf975300c6565e657410d7439c
claudiahW <murungiclaud@gmail.com>
2021-12-13 10:44:42 +0300

    define search path in urls

--- weaward/urls.py
+++ weaward/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('accounts/profile/', views.profile,name='profile'),
     path('post/project/', views.post, name = "post"),
-    path('update_profile/',views.update_profile, name='update_profile'),
+    path('update_profile/<int:id>',views.update_profile, name='update_profile'),
+    path('create_profile/',views.create_profile,name = 'create_profile'),
+    path('search/', views.search_project, name='search.post'),
 ]
